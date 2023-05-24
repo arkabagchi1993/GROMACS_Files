8ea3a017f5e4c956bdd24615509da27fa4eca052
arkabagchi1993 <134388389+arkabagchi1993@users.noreply.github.com>
2023-05-24 12:43:31 +0530

    Create matplot.py
    For XVG visualization

---
+++ matplot.py
@@ -0,0 +1,29 @@
+
+	import numpy as np
+
+	import matplotlib.pyplot as plt
+
+
+	# Load data from xvg file
+
+	data = np.loadtxt('<filename>.xvg', comments=['#', '@'])
+
+
+	# Extract columns from data
+
+	x = data[:, 0]
+
+	y = data[:, 1]
+
+
+	# Plot data using Matplotlib
+
+	plt.plot(x, y)
+
+	plt.xlabel('x label')
+
+	plt.ylabel('y label')
+
+	plt.title('Title of Plot')
+
+	plt.show()
