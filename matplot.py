dbd1b68e5577d6ceec5d5029b3cbebf1cbb1fa44
arkabagchi1993 <134388389+arkabagchi1993@users.noreply.github.com>
2023-05-24 12:47:12 +0530

    xvg visualization

--- matplot.py
+++ matplot.py
@@ -1,29 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-	import numpy as np
+# Load data from xvg file
+data = np.loadtxt('<filename>.xvg', comments=['#', '@'])
 
-	import matplotlib.pyplot as plt
+# Extract columns from data
+x = data[:, 0]
+y = data[:, 1]
 
-
-	# Load data from xvg file
-
-	data = np.loadtxt('<filename>.xvg', comments=['#', '@'])
-
-
-	# Extract columns from data
-
-	x = data[:, 0]
-
-	y = data[:, 1]
-
-
-	# Plot data using Matplotlib
-
-	plt.plot(x, y)
-
-	plt.xlabel('x label')
-
-	plt.ylabel('y label')
-
-	plt.title('Title of Plot')
-
-	plt.show()
+# Plot data using Matplotlib
+plt.plot(x, y)
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.title('Title of Plot')
+plt.show()
